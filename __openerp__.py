179f73025f33e420016351cbb5b245e7171e71a9
yassinerafa <yteimi01@gmail.com>
2015-07-15 18:49:09 +0000

    Source Code
    Source code for snippet

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Bootsnip By Yassine',
+    'description': 'This module provides a new snippet based on bootstrap',
+    'summary': 'This module provides a new snippet based on bootstrap',
+    'category': 'Theme',
+    'version': '1.0',
+    'website': '',
+    'author': 'TEIMI Yassine',
+    'depends': ['website'],
+    'data': [
+        'views/snippets.xml',
+		'views/assets.xml',
+    ],
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
